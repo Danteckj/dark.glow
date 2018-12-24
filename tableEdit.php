a9dc7b475c96918851c3d4b4479e1bb0069dc3b3
danteckj <danteckj@gmail.com>
2018-12-24 14:37:19 +0300

    1. Таблица по войне работает с аяксом

---
+++ tableEdit.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dante
+ * Date: 14.12.2018
+ * Time: 12:30
+ */
+include __DIR__ . '/simplePDOFunc.php';
+
+
+$pdomassive = [
+    'id' => NULL,
+    'war_date' => $_GET['warname'],
+
+    'damage' => $_GET['damage'],
+    'player_name' => $_GET['player'],
+    'input_id' => $_GET['inputId']
+];
+
+$sneakCount = numsqlins("SELECT COUNT(*) FROM War WHERE player_name = '" . $pdomassive['player_name'] . "' AND war_date = '" . $pdomassive['war_date'] . "'");
+if ($pdomassive['input_id'] <= $sneakCount) {
+    sqlSimpleInsert("UPDATE War SET damage =" . $pdomassive['damage'] . " WHERE ID=
+                                  (
+                                    SELECT id FROM (
+(SELECT id FROM War WHERE player_name = '" . $pdomassive['player_name'] . "' AND war_date = '" . $pdomassive['war_date'] . "'  ORDER BY id ASC  LIMIT " . $pdomassive['input_id'] . " )) AS t2  ORDER BY id DESC LIMIT 1
+                                  )");
+
+
+} else {
+
+    $ins = [
+        'id' => NULL,
+        'war_date' => $pdomassive['war_date'],
+        'damage' => $pdomassive['damage'],
+        'player_name' => $pdomassive['player_name']
+    ];
+    sqlInsertInTable($query = "INSERT INTO `War`" . "VALUES(:id,:war_date,:damage,:player_name)", $ins);
+    unset($ins);
+}
+
+$avgDamge = numsqlins("SELECT AVG(damage) FROM War WHERE war_date = '" . $pdomassive['war_date'] . "'");
+$sneaks = 120 - numsqlins("SELECT COUNT(*) FROM War WHERE war_date = '" . $pdomassive['war_date'] . "'");
+echo $sneaks . '.' . numsqlins("SELECT SUM(damage) FROM War WHERE  war_date = '" . $pdomassive['war_date'] . "'") . '.' . $avgDamge;
+unset($pdomassive);
+
+
+
+
+
+
+
+
+
